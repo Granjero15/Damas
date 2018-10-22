df46d34c9acbaa2254acfb9dc5ea7184f8f188fb
Granjero15 <javiermartinez5jmp@gmail.com>
2018-10-22 19:14:53 +0200

    modificacion

--- DamasJmpJsm/src/Tablero.java
+++ DamasJmpJsm/src/Tablero.java
@@ -14,20 +14,5 @@ public class Tablero {
 	 
  }
 
-public int getFilas() {
-	return filas;
-}
-
-public void setFilas(int filas) {
-	this.filas = filas;
-}
-
-public int getColumnas() {
-	return columnas;
-}
-
-public void setColumnas(int columnas) {
-	this.columnas = columnas;
-}
  
 }
