93823009e745cb8890703ab5575ccdbc29942151
Granjero15 <javiermartinez5jmp@gmail.com>
2018-10-22 19:13:04 +0200

    asd

--- DamasJmpJsm/src/Celda.java
+++ DamasJmpJsm/src/Celda.java
@@ -9,4 +9,5 @@ public class Celda {
 	public Celda() {
 	
 	}
+	
 }
--- DamasJmpJsm/src/Tablero.java
+++ DamasJmpJsm/src/Tablero.java
@@ -13,5 +13,21 @@ public class Tablero {
  public Tablero(int filas, int columnas) {
 	 
  }
+
+public int getFilas() {
+	return filas;
+}
+
+public void setFilas(int filas) {
+	this.filas = filas;
+}
+
+public int getColumnas() {
+	return columnas;
+}
+
+public void setColumnas(int columnas) {
+	this.columnas = columnas;
+}
  
 }
